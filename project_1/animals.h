257f4472d9542b400aac1014d4116652e58dda34
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2024-03-03 14:36:27 -0500

    Create animals.h

---
+++ project_1/animals.h
@@ -0,0 +1,21 @@
+/*
+int update_hare(int current_position)
+determines the new position of the hare after 1 second
+
+Parameters:
+    current_position: the current position of the hare
+Returns:
+    the new position of the hare after one time step
+*/
+extern int update_hare(int current_position);
+
+/*
+int update_tortoise(int current_position)
+determines the new position of the tortoise after 1 second
+
+Parameters:
+    the current position of the tortoise
+Returns:
+    the new position of the tortoise after one time step
+*/
+extern int update_tortoise(int current_position);
